2e40f7cc3e938a0128eaac980d3dfc0c64d851e2
Akira Ohgaki <akiraohgaki@gmail.com>
2024-10-16 19:00:52 +0900

    Update user.sh

--- devbox/setup/user.sh
+++ devbox/setup/user.sh
@@ -6,34 +6,34 @@ ENV_FILE="${HOME}/.localenv"
 
 touch "${ENV_FILE}"
 
-curl -fsSL https://sh.rustup.rs | RUSTUP_HOME="${INSTALL_PREFIX}" CARGO_HOME="${INSTALL_PREFIX}" sh -s -- --no-modify-path -y
+curl -fsSL https://sh.rustup.rs | RUSTUP_HOME="${INSTALL_PREFIX}" CARGO_HOME="${INSTALL_PREFIX}" sh -s -- -y --no-modify-path
 echo "export RUSTUP_HOME=${INSTALL_PREFIX}" >>"${ENV_FILE}"
 echo "export CARGO_HOME=${INSTALL_PREFIX}" >>"${ENV_FILE}"
 
-curl -fsSL https://deno.land/install.sh | DENO_INSTALL="${INSTALL_PREFIX}" sh -s --
+curl -fsSL https://deno.land/install.sh | DENO_INSTALL="${INSTALL_PREFIX}" sh -s -- -y --no-modify-path
 echo "export DENO_INSTALL=${INSTALL_PREFIX}" >>"${ENV_FILE}"
 
-curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n -o "${INSTALL_PREFIX}/bin/n" --create-dirs
+curl -fsSL -o "${INSTALL_PREFIX}/bin/n" --create-dirs https://raw.githubusercontent.com/tj/n/master/bin/n
 chmod 755 "${INSTALL_PREFIX}/bin/n"
 echo "export N_PREFIX=${INSTALL_PREFIX}" >>"${ENV_FILE}"
 N_PREFIX="${INSTALL_PREFIX}" "${INSTALL_PREFIX}/bin/n" lts
 
 ssh-keygen -t ed25519 -N '' -f "${HOME}/.ssh/id_ed25519"
 ssh-keygen -t rsa -N '' -f "${HOME}/.ssh/id_rsa"
-curl -fsSL "${CONFIGS_BASE_URL}/ssh/config" -o "${HOME}/.ssh/config"
+curl -fsSL -o "${HOME}/.ssh/config" "${CONFIGS_BASE_URL}/ssh/config"
 
-curl -fsSL "${CONFIGS_BASE_URL}/zsh/.zshrc" -o "${HOME}/.zshrc"
+curl -fsSL -o "${HOME}/.zshrc" "${CONFIGS_BASE_URL}/zsh/.zshrc"
 
-curl -fsSL "${CONFIGS_BASE_URL}/git/.gitconfig" -o "${HOME}/.gitconfig"
-curl -fsSL "${CONFIGS_BASE_URL}/git/.gitignore" -o "${HOME}/.gitignore"
+curl -fsSL -o "${HOME}/.gitconfig" "${CONFIGS_BASE_URL}/git/.gitconfig"
+curl -fsSL -o "${HOME}/.gitignore" "${CONFIGS_BASE_URL}/git/.gitignore"
 
-curl -fsSL "${CONFIGS_BASE_URL}/editorconfig/.editorconfig" -o "${HOME}/.editorconfig"
+curl -fsSL -o "${HOME}/.editorconfig" "${CONFIGS_BASE_URL}/editorconfig/.editorconfig"
 
-curl -fsSL "${CONFIGS_BASE_URL}/vim/.vimrc" -o "${HOME}/.vimrc"
+curl -fsSL -o "${HOME}/.vimrc" "${CONFIGS_BASE_URL}/vim/.vimrc"
 
-curl -fsSL "${CONFIGS_BASE_URL}/vscode/settings.json" -o "${HOME}/.vscode-remote/data/Machine/settings.json" --create-dirs
-curl -fsSL "${CONFIGS_BASE_URL}/vscode/settings.json" -o "${HOME}/.vscode-server/data/Machine/settings.json" --create-dirs
-curl -fsSL "${CONFIGS_BASE_URL}/vscode/extensions.json" -o "${HOME}/workspaces/.vscode/extensions.json" --create-dirs
+curl -fsSL -o "${HOME}/.vscode-remote/data/Machine/settings.json" --create-dirs "${CONFIGS_BASE_URL}/vscode/settings.json"
+curl -fsSL -o "${HOME}/.vscode-server/data/Machine/settings.json" --create-dirs "${CONFIGS_BASE_URL}/vscode/settings.json"
+curl -fsSL -o "${HOME}/workspaces/.vscode/extensions.json" --create-dirs "${CONFIGS_BASE_URL}/vscode/extensions.json"
 
 echo '' >>"${HOME}/.bashrc"
 echo "source ${ENV_FILE}" >>"${HOME}/.bashrc"
